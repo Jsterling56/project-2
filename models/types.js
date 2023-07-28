6cc3128442f495d76dc3e587bef8a49cb6098e1a
JSterling56 <jtsterlin@yahoo.com>
2023-07-27 20:58:17 -0400

    added database info, seed data

---
+++ models/types.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require ('sequelize');
+const sequelize = require('../config/connection.js');
+
+class types extends Model {}
+
+types.init(
+    {
+        id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+
+            autoIncrement: true
+        },
+        type_name:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        type_id:{
+            type: DataTypes.INTEGER,
+            references: { 
+                model: 'type', 
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'types',
+    }
+);
+
+module.exports = types;
